459bfa3032a7f3ae039bd41e1e3c206da34e48b6
Nicholas Jensen Hay <NicholasJensenHay@gmail.com>
2016-04-19 16:22:24 -0700

    Configure startup.cs

--- src/TodoList/Startup.cs
+++ src/TodoList/Startup.cs
@@ -19,7 +19,7 @@ namespace TodoList
         {
             var builder = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json");
-            Configuration = builder.Build();
+            Configuration = builder.Build(); 
         }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
@@ -35,7 +35,7 @@ namespace TodoList
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseStaticFiles();   
+            app.UseStaticFiles();
 
             app.UseIISPlatformHandler();
 
